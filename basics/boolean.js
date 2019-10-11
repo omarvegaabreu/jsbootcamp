c88c0f8dad453634f6e06edeb40a7c4bffdd09db
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-10-11 15:37:07 -0400

    conditional programing

--- basics/boolean.js
+++ basics/boolean.js
@@ -13,13 +13,27 @@
 // console.log(childAge);
 // console.log(seniorAge);
 
-let temp = 180;
+// let temp = 180;
 
-if (temp <= 32) {
-  console.log("its freezing");
-}
+// if (temp <= 32) {
+//   console.log("its freezing");
+// }
 
-if (temp >= 110) {
-  console.log("its is way to hot outside");
-  console.log("testing123");
-}
+// if (temp >= 110) {
+//   console.log("its is way to hot outside");
+//   console.log("testing123");
+// // }
+
+// let age = 6;
+
+// //if 7 or under print child pricing message
+
+// if (age <= 7) {
+//   console.log("you get a child discount");
+// }
+
+// if (age >= 65) {
+//   console.log("you get senior discount");
+// }
+
+// if 65 or older print senior discount
