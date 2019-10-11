f8daef8820993185e08bfef01f64cc60b6210f68
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-10-11 15:33:17 -0400

    basic if statements

---
+++ basics/boolean.js
@@ -0,0 +1,25 @@
+// //create age set to your age
+// let age = 1;
+// // calculate if child if 7 or under
+// let childAge = age <= 7;
+// //calculate is senior is 65 or older
+// let seniorAge = age >= 65;
+
+// if (true) {
+//   console.log('It is freezing outside.')
+
+// }
+
+// console.log(childAge);
+// console.log(seniorAge);
+
+let temp = 180;
+
+if (temp <= 32) {
+  console.log("its freezing");
+}
+
+if (temp >= 110) {
+  console.log("its is way to hot outside");
+  console.log("testing123");
+}
---
+++ basics/temperatureConverter.js
@@ -0,0 +1,14 @@
+let fahrenheit = 32;
+
+let celcius = ((fahrenheit - 32) * 5) / 9;
+
+let kelvin = ((fahrenheit + 459.67) * 5) / 9;
+
+console.log(
+  fahrenheit +
+    " degreess fahrenheit is equal to :" +
+    celcius +
+    " degrees celcius. It is also equal to: " +
+    kelvin +
+    " degreess kelvin."
+);
