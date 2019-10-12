4d2697e2846388102e0cc0de8d8df6c3c7b27c4b
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-10-11 21:04:14 -0400

    basics

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,34 @@
+// let isAccountLocked = 5;
+// let userRole = "user";
+
+// if (isAccountLocked === true) {
+//   console.log("your account is locked");
+// } else if (userRole === "admin") {
+//   console.log("Welcome admin");
+// } else {
+//   console.log("welcome!");
+// }
+
+// let temp = 114;
+
+// if (temp <= 45) {
+//   console.log("its freezing outside");
+// } else if (temp >= 115) {
+//   console.log("its hot outside!");
+// } else {
+//   console.log("Go for it. It is pretty nice out");
+// }
+
+//its freezing outside
+
+//its hot ouside
+
+//go for it
+
+let temp = 65;
+
+if (temp >= 65 && temp <= 90) {
+  console.log("its nice outside");
+} else {
+  console.log("test");
+}
---
+++ basics/logical-and-or-operators.js
@@ -0,0 +1,7 @@
+let temp = 65;
+
+if (temp >= 65 && temp <= 90) {
+  console.log("its nice outside");
+} else {
+  console.log("test");
+}
