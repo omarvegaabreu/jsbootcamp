da6e7c694690a01237b5accd90867967e15414ef
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-11-14 22:42:50 -0500

    basic dom manipulation

---
+++ notes-app/notes-app.js
@@ -0,0 +1,10 @@
+// const p = document.querySelector("p");
+
+// p.remove();
+const ps = document.querySelectorAll("p");
+
+ps.forEach(function(p) {
+  p.textContent = "******";
+});
+
+console.console.log();
