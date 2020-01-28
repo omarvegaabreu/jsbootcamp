409730bda31ea91d165ce707461f0fb72b8abcf3
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-01-28 15:04:12 -0500

    object oriented programming

--- oop/person.js
+++ oop/person.js
@@ -1,11 +1,34 @@
-const Person = function(firstName, lastName, age) {
+const Person = function(firstName, lastName, age, likes = []) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
+  this.likes = likes;
 };
 
-const me = new Person("Omar", "vega", 27);
-console.log(me);
+Person.prototype.getBio = function() {
+  let bio = `${this.firstName} likes ${this.age}  `;
 
-const meTwo = new Person("Rosely", "De Los Santos", 38);
-console.log(meTwo);
+  this.likes.forEach(like => {
+    bio += `${this.firstName} Likes ${like} `;
+  });
+
+  return bio;
+};
+
+Person.prototype.getAge = function() {
+  return `this is get age: ${this.age}`;
+};
+
+Person.prototype.setName = function(fullName) {
+  const names = fullName.split(" ");
+  this.firstName = names[0];
+  this.lastName = names[1];
+};
+
+const me = new Person("Omar", "Vega", 38, ["Jujitsu", "Playing", "family"]);
+me.setName("Alexis Turner");
+console.log(me.getBio());
+
+const meTwo = new Person("Rosely", "De los Santos", 38);
+console.log(meTwo.getBio());
+console.log(meTwo.getAge());
