84806abd47c06801e68ef3f262100856f1c7a9df
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-02-07 14:03:38 -0500

    learning class syntalx

--- oop/person.js
+++ oop/person.js
@@ -1,29 +1,36 @@
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
+class personConstructor {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} likes ${this.age}  `;
 
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} likes ${this.age}  `;
+    this.likes.forEach(like => {
+      bio += `${this.firstName} Likes ${like} `;
+    });
 
-  this.likes.forEach(like => {
-    bio += `${this.firstName} Likes ${like} `;
-  });
+    return bio;
+  }
+  getAge() {
+    return `this is get age: ${this.age}`;
+  }
 
-  return bio;
-};
+  setName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
 
-Person.prototype.getAge = function() {
-  return `this is get age: ${this.age}`;
-};
-
-Person.prototype.setName = function(fullName) {
-  const names = fullName.split(" ");
-  this.firstName = names[0];
-  this.lastName = names[1];
-};
+const newPerson = new personConstructor("Omar", "Vega", "38", [
+  "jujitsu",
+  "chess",
+  "MMA"
+]);
+console.log(newPerson);
 
 const me = new Person("Omar", "Vega", 38, ["Jujitsu", "Playing", "family"]);
 me.setName("Alexis Turner");
